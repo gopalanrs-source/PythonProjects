44d7cd3cb21e03ffbe379fb1b109d646bb467ae3
Gopalan R S <gopalan.rs@gmail.com>
2026-02-28 12:25:26 +0530

    basic calculator code to add 2 numbers

---
+++ Basics_calc.py
@@ -0,0 +1,6 @@
+#basic calculator code to add 2 numbers
+num1 = input("Enter the first number : ")
+num2 = input("Enter the second number : ")
+result = round(float(num1) + float(num2),2)
+print("the sum of numbers is : "+str(result))
+
