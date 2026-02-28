49dc2d0c8fccfd3a1d901650535756a87c9f3ad3
Gopalan R S <gopalan.rs@gmail.com>
2026-02-28 12:07:44 +0530

    changes to print start message

--- Basics_String_IO_process.py
+++ Basics_String_IO_process.py
@@ -5,8 +5,7 @@ year_dob = input("Enter year of DOB : ")
 patient_type = input("Enter patient type New/Old : ")
 current_year = 2026
 
-
+print("************   *****************")
 print("Name : "+first_name+ " "+last_name)
 print("Patient type : "+patient_type)
-print("Age : "+ str(current_year - int(year_dob)))
-      
+print("Age : "+ str(current_year - int(year_dob)))      
