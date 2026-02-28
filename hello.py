971abde29cf313f9d0af03c099ef2bdddf332dad
Gopalan R S <gopalan.rs@gmail.com>
2026-02-28 12:01:08 +0530

    Initial Python programs

---
+++ hello.py
@@ -0,0 +1,44 @@
+first_name = "John"
+last_name = "Smith"
+age = 25
+patient_type = "New" 
+
+#print("Name : "+first_name+" "+last_name)
+#print("Age : "+str(age))
+#print("Patient type : "+patient_type)   
+
+# find the average of 6 subject's marks provide by user
+#max_subject = input("Enter the maximum marks for subjects : ")
+#subject1 = input("Enter Math score : ")
+#subject2 = input("Enter Accounts score : ")
+#subject3 = input("Enter English score : ")
+#subject4 = input("Enter Business studies score : ")
+#subject5 = input("Enter Economics score : ")
+#subject6 = input("Enter Marketing score : ")
+
+#total = float(subject1)+float(subject2)+float(subject3)+float(subject4)+float(subject5)+float(subject6)
+#average_marks = (total/(float(max_subject)*6))*100
+#print(" Total Marks : "+str(total))
+#print(" Average Mark% : "+str(average_marks))
+
+course = "Python for Beginners"
+#print(course.upper())
+#print(course.lower())
+#print(course.find("for"))
+#print(course.replace("Beginners","Everyone"))
+#print(course.replace("Python","Java"))
+#print("Python" in course)
+#Strings are immutable in Python, which means that once a string is created, it cannot be changed. However, you can create a new string based on the original string with modifications.
+
+#x=10
+#print(x/3)
+#print(x//3)
+#print(x%3)
+#print(10**3)
+#print(10**0.5)
+#print(round(2.9))
+#print(round(2.2))
+#print(abs(-2.9))
+#print(abs(2.9))
+#print(pow(2,3))
+#print(pow(2,3,5))
