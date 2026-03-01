1fbb9c38ecd97a864a6d8a369194a628e6beb498
Gopalan R S <gopalan.rs@gmail.com>
2026-03-01 15:07:34 +0530

    string operations practise

---
+++ basucs_string_op.py
@@ -0,0 +1,10 @@
+course = "Python for Beginners"
+print(course.find("for"))
+course2 = course.replace("for", "4")
+course3 = course.replace("Beginners","Everyone")
+print(course)
+print(course2)
+print(course3)
+course = course2+course3
+print(course)
+#string is immutable in python. We cannot change the string but we can create a new string by concatenating the old string with the new string.
