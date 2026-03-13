5b79758cc6a2362788fd69435ecdd598900932a2
Gopalan R S <gopalan.rs@gmail.com>
2026-03-13 10:52:48 +0530

    ifelse-elif statements

---
+++ ifelse.py
@@ -0,0 +1,13 @@
+age = int(input("Enter Student age : "))
+
+if age >= 14 and age <= 18:
+    print("Studen is in High School")
+elif age > 18 and age <=22:
+     print("Student is in college")   
+elif age <14 and age >= 5:
+     print("Student is in middle school")
+elif age < 5:
+     print("Student is in elementary school")
+else :
+     print("Student is out of school")     
+
