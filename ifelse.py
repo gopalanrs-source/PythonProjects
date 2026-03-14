60666776accac886f11e1d87007b5ceb1b40358e
Gopalan R S <gopalan.rs@gmail.com>
2026-03-14 19:22:58 +0530

    tab newline learnt

--- ifelse.py
+++ ifelse.py
@@ -1,4 +1,4 @@
-age = int(input("Enter Student age : "))
+age_val = int(input("Enter Student age : "))
 
 if age_val >= 14 and age_val <= 18:
     print("Studen is in High School")
@@ -13,6 +13,18 @@ else :
 
 print(age_val if age_val <= 18 else "Student is out of school")
 print("ternary operation in python" if age_val > 1 else "else condition of ternary operation")
+
+multi_line = """ hello
+                 world!"""
+print(multi_line)
+
+escape_char = "I'm learning python \t And it's fun \n Join me!"
+escape_char2 = 'I\'m learning python'
+
+print(escape_char)
+print(escape_char2)
+
+
 
 
       
