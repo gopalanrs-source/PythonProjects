eab828b3288b5d5455201f9d07b3a59c5b0f79c8
Gopalan R S <gopalan.rs@gmail.com>
2026-03-14 18:53:45 +0530

    learnt multiple replacement of varaibale names with ctrl+d

--- ifelse.py
+++ ifelse.py
@@ -1,18 +1,18 @@
 age = int(input("Enter Student age : "))
 
-if age >= 14 and age <= 18:
+if age_val >= 14 and age_val <= 18:
     print("Studen is in High School")
-elif age > 18 and age <=22:
+elif age_val > 18 and age_val <=22:
      print("Student is in college")   
-elif age <14 and age >= 5:
+elif age_val <14 and age_val >= 5:
      print("Student is in middle school")
-elif age < 5:
+elif age_val < 5:
      print("Student is in elementary school")
 else :
      print("Student is out of school")   
 
-print(age if age <= 18 else "Student is out of school")
-print("ternary operation in python" if age > 1 else "else condition of ternary operation")
+print(age_val if age_val <= 18 else "Student is out of school")
+print("ternary operation in python" if age_val > 1 else "else condition of ternary operation")
 
 
       
