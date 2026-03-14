ec128673da5edb0faa87794b86c1951d5835875d
Gopalan R S <gopalan.rs@gmail.com>
2026-03-14 20:09:00 +0530

    string format + indexes

---
+++ String_format.py
@@ -0,0 +1,17 @@
+# name = "menu".upper()
+# print(name.center(20,"*"))
+# print("Coffee".ljust(20,".") + "$1.00".rjust(4))
+# print("Tea".ljust(20,".") + "$1.50".rjust(4))
+# print("Cake".ljust(20,".") + "$4.00".rjust(4))
+
+name = "hello world"
+print(name[:])
+print(name[0:])
+print(name[0:7])
+print(name[-1])
+print(name[1:7])
+print(name[1:15])
+print(name[1:-1])
+
+
+
