2bcf659e0d802870c6c933fa985403aa66d0d7b9
Gopalan R S <gopalan.rs@gmail.com>
2026-03-15 21:05:04 +0530

    math lib methods

---
+++ complex_num.py
@@ -0,0 +1,10 @@
+a = 5+3j
+print(type(a))
+print(a.real)
+print(a.imag)
+
+
+b = -4.56
+print(abs(b))
+print(abs(round(b,1)))
+
---
+++ math_lib.py
@@ -0,0 +1,9 @@
+import math
+
+gpa = 3.86
+print(math.ceil(gpa))
+print(math.floor(gpa))
+print(math.sqrt(100))
+print(math.pow(2,3))
+print(math.pi)
+print(math.e)
