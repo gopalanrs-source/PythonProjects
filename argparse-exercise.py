065bb20a9e5b08e99dfaa46ac0d20a1f0c3f9bd4
Gopalan R S <gopalan.rs@gmail.com>
2026-03-26 19:17:18 +0530

    argparse with method for commandline execution

---
+++ argparse-exercise.py
@@ -0,0 +1,32 @@
+import argparse
+
+def hello(name, lang):
+    greetings = {
+        "English": "Hello",
+        "Spanish": "Hola",
+        "French": "Bonjour"
+    }
+    msg = f"{greetings[lang]} {name}!"
+    print(msg)
+
+parser = argparse.ArgumentParser(description="A simple command-line tool to demonstrate argparse.")
+
+# parser.add_argument("name", type=str, help="Your name")
+# parser.add_argument("--age", type=int, help="Your age", default=0)
+# parser.add_argument("--greet", action="store_true", help="Whether to greet the user")
+# args = parser.parse_args()
+# if args.greet:
+#     print(f"Hello, {args.name}!")
+# if args.age > 0:
+#     print(f"You are {args.age} years old.")
+
+parser.add_argument("-n", "--name", metavar="name", required=True, help="Your name")
+parser.add_argument("-l", "--lang", metavar="Language", choices=["English","Spanish","French"], help="Language to greet in (English, Spanish, French)", default="English")
+
+
+args = parser.parse_args()
+
+hello(args.name, args.lang)
+
+# msg = f"Hello, {args.name}!"
+# print(msg)
