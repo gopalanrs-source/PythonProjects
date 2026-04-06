680b0ac2f29acd1cf24fc4247bee394d35b8b94d
Gopalan R S <gopalan.rs@gmail.com>
2026-04-06 22:37:30 +0530

    waether-api request

---
+++ weather.py
@@ -0,0 +1,20 @@
+import requests
+from dotenv import load_dotenv
+import os
+from pprint import pprint
+
+load_dotenv()
+
+city = input("Enter the city name: ")
+
+
+
+def get_weather(city):
+    print("Getting weather data for", city)
+
+    request_url = f'https://api.openweathermap.org/data/3.0/onecall?&exclude={part}&q={city}&appid={os.getenv("API_KEY")}&units=metric'
+
+    print("Request URL:", request_url)
+
+
+get_weather(city)
