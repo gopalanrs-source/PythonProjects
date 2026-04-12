bf41586a18dc17bdf0e8f39c0721dc46c723ce41
Gopalan R S <gopalan.rs@gmail.com>
2026-04-12 12:43:45 +0530

    add remove collumns

--- df_sample.py
+++ df_sample.py
@@ -34,14 +34,43 @@ print(df)
 
 # update rows
 df.loc[2] = ['John', 'Smith', 'john.smith@email.com']
-print(df)
+#print(df)
 df.loc[2, ['last', 'email']] = ['Doe', 'JohnDow@email.com']
-print(df)
+#print(df)
 df.at[2, 'email'] = 'JohnDoe@email.com'
+#print(df)
+
+
+#print(df['email'].apply(lambda x: x.upper()))
+#print(df['email'].apply(len))
+
+df['full_name'] = df['first'] + ' ' + df['last']
+# print(df)
+df.drop(columns=['first','last'], inplace=True)
+# print(df)
+df[['first','last']] = df['full_name'].str.split(' ', expand=True)
+# print(df)
+
+print(df)                   
+df.loc[len(df)] = {'email': 'tony.stark@email.com','full_name': 'Tony Stark','first': 'Tony', 'last': 'Stark'}
+print(df)                   
+new_row = pd.DataFrame([{'first': 'Banner'}]) 
+df = pd.concat([df, new_row], ignore_index=True)
 print(df)
 
 
-print(df['email'].apply(lambda x: x.upper()))
-print(df['email'].apply(len))
+people2 = {
+    "first" : ["Black", "Captain"],
+    "last" : ["Widow", "America"],
+    "email" : ["black.widow@gmail.com", "captain.america@gmail.com"]
+}
 
-
+df2 = pd.DataFrame(people2)
+print(df2)
+df = pd.concat([df, df2], ignore_index=True)
+print(df)
+df.drop(index=4, inplace=True)
+print(df)
+filt = (df['last'] == 'Doe') 
+df.drop(index=df[filt].index, inplace=True)
+print(df)
