568a00143c0594053e79bb2e188133439ec8f884
Gopalan R S <gopalan.rs@gmail.com>
2026-04-13 20:36:47 +0530

    2 files comitted - concat, sort etc

--- df_stackoverflow_exercise.py
+++ df_stackoverflow_exercise.py
@@ -25,7 +25,50 @@ df.sort_values(by=['Country', 'SalaryUSD'], ascending=[True, False], inplace=Tru
 # print(df['SalaryUSD'].nlargest(10))
 # print(df.nlargest(10, 'SalaryUSD')[['Country','SalaryUSD', 'EdLevel', 'Employment']])
 # print(df.nsmallest(10, 'SalaryUSD')[['Country','SalaryUSD']])
-print(df.median())
+# print(df['SalaryUSD'].median())
+#print(df.median())
+# print(df.describe())
+# print(df['Hobbyist'].value_counts())
+# print(df['SocialMedia'].value_counts(normalize=True))
+
+country_grp = df.groupby('Country')
+# print(country_grp['SalaryUSD'].median().sort_values(ascending=False))
+# print(country_grp.get_group('United States'))
+
+# filt2 = df['Country'] == 'India'
+# print(df.loc[filt2]['SocialMedia'].value_counts())
+
+# print(country_grp['SocialMedia'].value_counts().head(50))
+# print(country_grp['SocialMedia'].value_counts().loc['India'])
+
+# print(country_grp['SalaryUSD'].agg(['median', 'mean', 'min', 'max', 'count']))
+
+# print(country_grp['SalaryUSD'].median().loc['India'])
+
+filt = df['Country'] == "India"
+# print(df.loc[filt]['LanguageWorkedWith'].str.contains('Python').value_counts())
+# print(df.loc[filt]['LanguageWorkedWith'].str.contains('Python').sum())   # sum will not work on groupby object, it will work on series object. So we need to filter the dataframe first and then apply the sum function on the series object.
+
+python_users = country_grp['LanguageWorkedWith'].apply(lambda x: x.str.contains('Python').sum())
+country_respondents = df['Country'].value_counts()
+# python_pct = (python_users / country_respondents) * 100
+# print(python_pct.sort_values(ascending=False))
+
+python_df = pd.concat([country_respondents, python_users], axis='columns', sort=False)
+print(python_df.head(5))
+python_df.columns = ['TotalRespondents', 'PythonUsers'] 
+python_df['PythonPct'] = (python_df['PythonUsers'] / python_df['TotalRespondents']) * 100
+print(python_df.sort_values(by='PythonPct', ascending=False).head(10))
+print(python_df.loc['India'])
+
+
+
+
+
+
+
+
+      
 
       
 
